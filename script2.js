55fad1ecfa4d55dc3a6b6d291896c2b6aabc17de
Philippa Davies <philippadavies603@gmail.com>
2023-06-22 22:32:02 +1000

    java code structure completed, still need text content to be written (e.g questions, .textContent)

--- script2.js
+++ script2.js
@@ -1,34 +1,48 @@
+let quizEl = document.querySelector(".quiz-container");
 const questionEl = document.querySelector("#question");
 let answerElA = document.querySelector("#answerA");
 let answerElB = document.querySelector("#answerB");
 let answerElC = document.querySelector("#answerC");
 let timeEl = document.querySelector("#timer")
+let finishButton = document.querySelector("#finish")
+let seeScoresButton = document.querySelector("#scores")
+let scoreList = document.querySelector("#score-list")
+let initialsEl = document.querySelector("#initials")
+
+// Timer
+var secondsLeft = 300;
+
+var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "You have " + secondsLeft + " seconds left";
+    if (secondsLeft < 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+    }
+}, 1000);
 
 
-var secondsLeft = 30;
-function setTime() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
-        }
-    }, 1000);
-}
-
-function sendMessage() {
-    timeEl.textContent = "Time is up!";
-}
+//Hide buttons before game starts
+timeEl.style.display = 'none';
+seeScoresButton.style.display = 'none';
+finishButton.style.display = 'none';
+answerElA.style.display = 'none';
+answerElB.style.display = 'none';
+answerElC.style.display = 'none'
+initialsEl.style.display = 'none'
 
 
+
+//Questions 
 let questionsList = [
-    { title: "How do you change the colour of an H1 element in CSS?", answerA: "color:", answerB: "font-color", answerC: "h1-color:", correctAnswer: "color" },
-    { title: "How do you change the colour of an H22222 in CSS?", answerA: "co6666or:", answerB: "font-color", answerC: "h1-color:", correctAnswer: "color" },
-    { title: "How do you change the colour of an H33333in CSS?", answerA: "colo65:", answerB: "font-color", answerC: "h1-color:", correctAnswer: "color" },
-    { title: "How do you change the colour of an H1 element in CSS?", answerA: "col444or:", answerB: "font-color", answerC: "h1-color:", correctAnswer: "color" },
+    { title: "How do you change the colour of an H1 element in CSS?", answerA: "color", answerB: "font-color", answerC: "h1-color:", correctAnswer: "color" },
+    { title: "How do you change the colour of an H22222 in CSS?", answerA: "co6666or", answerB: "font-color", answerC: "h1-color:", correctAnswer: "co6666or" },
+    { title: "How do you change the colour of an H33333in CSS?", answerA: "colo65", answerB: "font-color", answerC: "h1-color:", correctAnswer: "color" },
+    { title: "How do you change the colour of an H1 element in CSS?", answerA: "col444or", answerB: "font-color", answerC: "h1-color:", correctAnswer: "color" },
 ]
 
+
+// Display Questions
 let questionIndex = 0;
 
 function displayQuestion() {
@@ -40,79 +54,187 @@ function displayQuestion() {
 }
 
 
+//Start Quiz
+let startEl = document.querySelector("#start");
+
+startEl.addEventListener("click", function () {
+    secondsLeft = 50;
+    displayQuestion();
+    startEl.style.display = 'none'
+    seeScoresButton.style.display = 'none';
+    finishButton.style.display = 'none';
+    timeEl.style.display = 'inline';
+    answerElA.style.display = 'inline';
+    answerElB.style.display = 'inline';
+    answerElC.style.display = 'inline'
+
+}
+);
+
+//Cycle Through Questions
 let score = 0;
+
 
 answerElA.addEventListener("click", btnPressA);
 function btnPressA() {
     var userSelection = questionsList[questionIndex - 1].answerA;
-    var answer = value.questionsList[questionIndex - 1].correctAnswer;
-    console.log(answer)
-
-    if ((userSelection == answer) & secondsLeft > 0) {
-        score++
+    var answer = questionsList[questionIndex - 1].correctAnswer;
+    if ((userSelection == answer) && secondsLeft > 0) {
+        score++;
         console.log("yes");
-    } else
+        console.log(score)
+    } else if ((userSelection != answer) && secondsLeft > 0) {
+        secondsLeft -= 10;
+        timeEl.append("        10 secs lost!")
         console.log("no")
-    if (questionIndex < 3) {
-        displayQuestion()
-    } else {
-        questionEl.textContent = "end"
+    }
+
+    if (questionIndex < 4 && secondsLeft > 0) {
+        displayQuestion();
+    } else if (secondsLeft < 0 && userSelection !== answer) {
+        secondsLeft -= 10;
+        if (secondsLeft == 0 || secondsLeft < 0) {
+            secondsLeft = 0
+        }
+        timeEl.textContent = "Game Over! You have " + secondsLeft + " seconds left";
+        end()
+    } else if (secondsLeft < 0 && userSelection == answer) {
+        end()
     }
 }
+
 
 answerElB.addEventListener("click", btnPressB);
 function btnPressB() {
     var userSelection = questionsList[questionIndex - 1].answerB;
     var answer = questionsList[questionIndex - 1].correctAnswer;
-    console.log(answer)
-
-    if ((userSelection.valueOf == answer.valueOf) & secondsLeft > 0) {
-        score++
+    if ((userSelection == answer) && secondsLeft > 0) {
+        score++;
         console.log("yes");
-    } else
+        console.log(score)
+    } else if ((userSelection != answer) && secondsLeft > 0) {
+        secondsLeft -= 10;
+        timeEl.append("        10 secs lost!")
         console.log("no")
-    if (questionIndex < 3) {
-        displayQuestion()
-    } else {
-        questionEl.textContent = "end"
+    }
+
+    if (questionIndex < 4 && secondsLeft > 0) {
+        displayQuestion();
+    } else if (secondsLeft < 0 && userSelection !== answer) {
+        secondsLeft -= 10;
+        if (secondsLeft == 0 || secondsLeft < 0) {
+            secondsLeft = 0
+        }
+        timeEl.textContent = "Game Over! You have " + secondsLeft + " seconds left";
+        end()
+    } else if (secondsLeft < 0 && userSelection == answer) {
+        end()
+
     }
 }
+
 
 answerElC.addEventListener("click", btnPressC);
 function btnPressC() {
     var userSelection = questionsList[questionIndex - 1].answerC;
     var answer = questionsList[questionIndex - 1].correctAnswer;
-    console.log(answer)
-
-    if ((userSelection.valueOf == answer.valueOf) & secondsLeft > 0) {
-        score++
+    if ((userSelection == answer) && secondsLeft > 0) {
+        score++;
         console.log("yes");
-    } else
+        console.log(score)
+    } else if ((userSelection != answer) && secondsLeft > 0) {
+        secondsLeft -= 10;
+        timeEl.append("        10 secs lost!")
         console.log("no")
-    if (questionIndex < 3) {
-        displayQuestion()
-    } else {
-        questionEl.textContent = "end"
+    }
+    if (questionIndex < 4 && secondsLeft > 0) {
+        displayQuestion();
+    } else if (secondsLeft < 0 && userSelection !== answer) {
+        secondsLeft -= 10;
+        if (secondsLeft == 0 || secondsLeft < 0) {
+            secondsLeft = 0
+        }
+        timeEl.textContent = "Game Over! You have " + secondsLeft + " seconds left";
+        end()
+    } else if (secondsLeft < 0 && userSelection == answer) {
+        end()
+
     }
 }
 
-//Start Quiz
-let startEl = document.querySelector("#start");
-
-startEl.addEventListener("click", function () {
-    setTime();
-    displayQuestion();
-    startEl.style.display = 'none'
-
+//Stop Timer
+function stopTimer() {
+    clearInterval(timerInterval)
 }
-);
 
 
+// End Quiz
+function end() {
+    answerElA.style.display = 'none'
+    answerElB.style.display = 'none'
+    answerElC.style.display = 'none'
+    seeScoresButton.style.display = 'inline';
+    finishButton.style.display = 'inline';
+    initialsEl.style.display = 'inline'
+    quizEl.textContent = "Your score is " + score;
+    stopTimer()
+}
 
 
+// Score saving
+let responseArray = []
+
+function saveScore() {
+    let enteredInitials = document.getElementById("userInit").value;
+
+    let responses = {
+        score: score,
+        initials: enteredInitials,
+    }
+
+    responseArray.push(responses)
+
+    console.log(responseArray)
+
+    localStorage.setItem("responses", JSON.stringify(responseArray));
+}
 
 
+finishButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    saveScore();
+});
 
-//when all questions are answered, timer stops
-//if user gets question wrong, minus 10 seconds from timer
-//score logged
+
+function saveSave() {
+    var getResponse = JSON.parse(localStorage.getItem("responses"));
+    if (getResponse !== null) {
+        responseArray = getResponse;
+    }
+}
+
+saveSave()
+
+
+function renderScores() {
+
+    for (let index = 0; index < responseArray.length; index++) {
+        const element = responseArray[index];
+        var li = document.createElement('li');
+        li.innerText = element.initials + "       " + element.score;
+        scoreList.appendChild(li);
+    }
+}
+
+
+seeScoresButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    renderScores();
+});
+
+
+function sendMessage() {
+    end();
+    secondsLeft = 0
+    timeEl.textContent = "Game Over! You have " + secondsLeft + " seconds left";
+}
